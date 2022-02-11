f5eb4fa14b0949a58d77b44119bea1fdf35f38bc
Tiago Jacobs <tiago.jacobs@gmail.com>
2022-02-11 19:30:39 -0300

    Change main screen text

--- App.js
+++ App.js
@@ -65,25 +65,16 @@ const App: () => Node = () => {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
+          <Section title="Welcome">
+            Welcome to first version of <Text style={styles.highlight}>BigBlueButton Mobile</Text>
           </Section>
           <Section title="Debug">
             <DebugInstructions />
           </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
         </View>
       </ScrollView>
     </SafeAreaView>
