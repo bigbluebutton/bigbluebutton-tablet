05c053beb3b45fa9bc7e630ff8d63db0f1870675
Tiago Jacobs <tiago.jacobs@gmail.com>
2022-02-12 01:05:48 -0300

    Add logs to events of ReplayKit2

--- ios/BroadcastUpload/SampleHandler.swift
+++ ios/BroadcastUpload/SampleHandler.swift
@@ -11,18 +11,22 @@ class SampleHandler: RPBroadcastSampleHandler {
 
     override func broadcastStarted(withSetupInfo setupInfo: [String : NSObject]?) {
         // User has requested to start the broadcast. Setup info from the UI extension can be supplied but optional.
+      print("replayKit2 event :: broadcastStarted")
     }
     
     override func broadcastPaused() {
         // User has requested to pause the broadcast. Samples will stop being delivered.
+      print("replayKit2 event :: broadcastPaused")
     }
     
     override func broadcastResumed() {
         // User has requested to resume the broadcast. Samples delivery will resume.
+      print("replayKit2 event :: broadcastResumed")
     }
     
     override func broadcastFinished() {
         // User has requested to finish the broadcast.
+      print("replayKit2 event :: broadcastFinished")
     }
     
     override func processSampleBuffer(_ sampleBuffer: CMSampleBuffer, with sampleBufferType: RPSampleBufferType) {
