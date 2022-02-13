70b0d8a09639aaa7bbec8a4adac66ebda638d18a
Tiago Jacobs <tiago.jacobs@gmail.com>
2022-02-13 12:15:36 -0300

    Fix formatting

--- react/BroadcastPicker.js
+++ react/BroadcastPicker.js
@@ -2,7 +2,11 @@ import React from 'react';
 import SystemBroadcastPicker from './ios-native-components/BBBN_SystemBroadcastPicker';
 
 const BroadcastPicker = ({children, title}): Node => {
-  return <SystemBroadcastPicker style={{marginTop: 50, height: 50, widht: 50, backgroundColor: '#EEE'}} />;
+  return (
+    <SystemBroadcastPicker
+      style={{marginTop: 50, height: 50, widht: 50, backgroundColor: '#EEE'}}
+    />
+  );
 };
 
 export default BroadcastPicker;
