8e3166491405fa7d00cd9cbdea6dcf1f4727761a
Tiago Jacobs <tiago.jacobs@gmail.com>
2022-03-01 18:38:25 -0300

    Apply fixes for IOS

--- App.tsx
+++ App.tsx
@@ -6,7 +6,12 @@ import { PortalContextContainer } from './react-native/app/contexts/portals/cont
 import { LogBox } from 'react-native';
 import { Bootstrap } from './react-native/bootstrap/start/component';
 
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome5';
 
+FontAwesomeIcon.getStyledIconSet('brand').loadFont();
+FontAwesomeIcon.getStyledIconSet('light').loadFont();
+FontAwesomeIcon.getStyledIconSet('regular').loadFont();
+FontAwesomeIcon.getStyledIconSet('solid').loadFont();
 
 
 export default function App() {
