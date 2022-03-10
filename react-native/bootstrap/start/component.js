b8836f83a25ba6dd3531bfaa0b2b601f58728b5e
Gustavo Emanuel Farias Rosa <gustavoemanuelfariasrosa@Gustavos-Mac-mini.local>
2022-03-10 14:31:28 -0300

    splash, icon and safeareaview

--- react-native/bootstrap/start/component.js
+++ react-native/bootstrap/start/component.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Routes } from '../../app/routes/component';
 import { PortalContextContainer } from '../../app/contexts/portals/container';
+import { SafeAreaView } from 'react-native';
 
 
 //This file is used to envolve app in contexts, libs, etc..
 export const Bootstrap = ()=>{
     return (  
         <PortalContextContainer>
+            
             <Routes/>
+        
         </PortalContextContainer>                    
     )
 }
