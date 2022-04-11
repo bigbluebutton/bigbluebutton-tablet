622da30e590dd3f7dc72e680f07f15f8a220b94d
Tiago Jacobs <tiago.jacobs@gmail.com>
2022-04-11 13:31:36 -0300

    Add missing import and fix Info.plist permissions

--- ios/BigBlueButton Broadcast/SampleHandler.swift
+++ ios/BigBlueButton Broadcast/SampleHandler.swift
@@ -1,8 +1,16 @@
-import bigbluebutton_mobile_sdk_common
+//
+//  SampleHandler.swift
+//  BigBlueButtonMobileSdkBroadcastExample
+//
+//  Created by Tiago Daniel Jacobs on 15/02/22.
+//
+
+import ReplayKit
+import bigbluebutton_mobile_sdk_broadcast_upload_extension
 
 class SampleHandler: BBBSampleHandler {
-  override func broadcastStarted(withSetupInfo setupInfo: [String : NSObject]?) {
-    super.setAppGroupName(appGroupName: Constants.appGroupName)
-    super.broadcastStarted(withSetupInfo: setupInfo)
-  }
+    override func broadcastStarted(withSetupInfo setupInfo: [String : NSObject]?) {
+      super.setAppGroupName(appGroupName: Constants.appGroupName)
+      super.broadcastStarted(withSetupInfo: setupInfo)
+    }
 }
