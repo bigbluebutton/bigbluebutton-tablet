99fcc0c12991fd9e5056923848814b55484a6c31
Tiago Jacobs <tiago.jacobs@gmail.com>
2022-04-12 11:42:04 -0300

    Update en-US.ts

--- react-native/app/translations/resources/en-US.ts
+++ react-native/app/translations/resources/en-US.ts
@@ -1,13 +1,13 @@
 export default {
-        "mobileApp.portals.list.empty.label": "There aren't portals.",
-        "mobileApp.portals.list.add.button.label": "New Portal",
+        "mobileApp.portals.list.empty.label": "Add your first portal.",
+        "mobileApp.portals.list.add.button.label": "Add portal",
         "mobileApp.portals.fields.name.label": "Portal Name",
         "mobileApp.portals.fields.name.placeholder": "BigBlueButton demo",
         "mobileApp.portals.fields.url.label": "Server URL",
         "mobileApp.portals.fields.url.placeholder": "https://demo.bigbluebutton.org",
-        "mobileApp.portals.addPortalPopup.confirm.button.label": "Add Portal",
+        "mobileApp.portals.addPortalPopup.confirm.button.label": "Save",
         "mobileApp.portals.drawerNavigation.button.label": "Portals",
         "mobileApp.portals.addPortalPopup.validation.emptyFilds": "Empty Fields",
-        "mobileApp.portals.addPortalPopup.validation.portalNameAlreadyExists": "Name Already Exists",
-        "mobileApp.portals.addPortalPopup.validation.urlInvalid": "Error trying to load the page - check if address is correct and your network is connected"
+        "mobileApp.portals.addPortalPopup.validation.portalNameAlreadyExists": "Name already in use",
+        "mobileApp.portals.addPortalPopup.validation.urlInvalid": "Error trying to load the page - check URL and network connection"
 }
