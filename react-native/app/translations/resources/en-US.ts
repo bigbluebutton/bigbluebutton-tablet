5532776532129778aae3bc351e690a943577c24b
Tiago Jacobs <tiago.jacobs@gmail.com>
2022-04-13 11:56:56 -0300

    Refactor translation keys

--- react-native/app/translations/resources/en-US.ts
+++ react-native/app/translations/resources/en-US.ts
@@ -1,5 +1,6 @@
 export default {
-        "mobileApp.portals.list.empty.label": "Add your first portal.",
+        "mobileApp.portals.list.empty.addFirstPortal.label": "Add your first portal using the button above,",
+        "mobileApp.portals.list.empty.orUseOurDemoServer.label": "or use our demo server.",
         "mobileApp.portals.list.add.button.label": "Add portal",
         "mobileApp.portals.fields.name.label": "Portal Name",
         "mobileApp.portals.fields.name.placeholder": "BigBlueButton demo",
@@ -7,7 +8,7 @@ export default {
         "mobileApp.portals.fields.url.placeholder": "https://demo.bigbluebutton.org",
         "mobileApp.portals.addPortalPopup.confirm.button.label": "Save",
         "mobileApp.portals.drawerNavigation.button.label": "Portals",
-        "mobileApp.portals.addPortalPopup.validation.emptyFilds": "Empty Fields",
+        "mobileApp.portals.addPortalPopup.validation.emptyFields": "Empty Fields",
         "mobileApp.portals.addPortalPopup.validation.portalNameAlreadyExists": "Name already in use",
         "mobileApp.portals.addPortalPopup.validation.urlInvalid": "Error trying to load the page - check URL and network connection"
 }
